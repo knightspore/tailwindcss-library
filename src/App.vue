59b7a70f921dc83c42d662f0b172e9b3d5132743
knightspore <cslemon96@gmail.com>
2020-08-23 20:39:15 +0200

    init and add accommodation card

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h2 class="text-xs font-black mb-2">ACCOMMODATION CARDS</h2>
+    <div class="flex justify-center">
+    <AccommodationCard
+      title="Kommetjie"
+      rating="4.2"
+      description="Tucked away in the quietest corner of the peninsula, an hour from the city center. " 
+      img="https://q-cf.bstatic.com/images/hotel/max1024x768/143/143105271.jpg"/>
+
+    <AccommodationCard
+      title="Scarborough"
+      rating="3.8"
+      description="Secluded and surrounded by endless horizons, lapping waves and stunning sunsets." 
+      img="https://a0.muscache.com/im/pictures/f919f8ac-b3c3-4ef4-a398-789778b0bf86.jpg"/>
+
+    <AccommodationCard 
+      title="Elands Bay"
+      rating="4.0"
+      description="One for the surfers - a crayfishing town with the best waves between Cape Town & Namibia."
+      img="https://www.forkwest.co.za/media/img/mg/shutterstock_31491526.jpg"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import AccommodationCard from './components/cards/AccommodationCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AccommodationCard
+  }
+}
+</script>
+
+<style src="./index.css">
+</style>
---
+++ src/components/cards/AccommodationCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mx-2 text-white text-left m-auto bg-red-900 rounded-md shadow-lg max-w-xs overflow-hidden">
+        <img class="object-cover w-full h-48" :src=img alt="">
+        <div class="pt-2 px-4 pb-6 border-t-8 border-red-800">
+            <h1 class="text-2xl">{{title}}</h1>
+            <span class="text-red-300 text-xs font-light">User Rating: <span class="font-bold text-red-100">{{rating}}</span> / 5</span>
+            <p class="mt-2 mb-5">{{description}}</p>
+            <span class="text-gray-100 text-xs font-black p-2 bg-red-700 hover:bg-red-600 hover:shadow-md rounded-md">Check Availability</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AccommodationCard',
+    props: [
+        'title',
+        'description',
+        'rating',
+        'img'
+    ]
+}
+</script>
